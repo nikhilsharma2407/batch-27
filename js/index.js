0e19116cf2b24c3a25c85851bd674bf44f44ceac
Nikhil Sharma <nikhil.sharma2407@gmail.com>
2023-02-18 22:39:08 +0530

    Day-5 arrow Fn, this binding, map,filter reduce, ES6 spread operator Object, prototype,ES6 classes

--- js/index.js
+++ js/index.js
@@ -89,3 +89,211 @@ console.log(numStr.join(''))
 // // console.log(parseInt('abcd'));
 // // console.log(parseInt('0xc23',16));
 // console.log(parseInt('abcd'));
+
+
+// boolean - true/false;
+// Truthy/ Non - Truthy
+
+/**
+ * Non-Truthy
+ * 0
+ * Empty string "", '',``
+ * null, undefined
+ * NaN
+ */
+
+/**
+ * Truthy
+ * empty array  []
+ * empty object {}
+ */
+// 
+
+
+if([]){
+    console.log("truthy");
+}
+else{
+    console.log("falsy")
+}
+
+
+// null, undefined
+
+let loggedInUser = {username:"Nikhil"};
+
+loggedInUser = null;
+
+let err = 'login err';
+err = null;
+
+// ==, ===
+// strict equality, check the value + data type
+
+1 == '1' //true
+1 === '1' //false
+
+0 == false //true
+0 === false //false
+null == undefined //true
+
+function sumFn(num1,num2){
+    // this own scope
+    return num1+num2;
+}
+
+// const sumArrowFn = (num1,num2)=>{
+//     // this -> parent scope
+//     return num1+num2
+// }
+// only if we want to return from the function directly
+const sumArrowFn = (num1,num2)=>num1+num2;
+
+console.log(sumArrowFn(1,2,3,4,4,5))
+
+// for single argument, we can even skip () prenthesis
+const fn = name =>`Hello ${name}`;
+
+console.log(fn("Nikhil"))
+
+
+// arrays 
+
+const array = [1,"string",true,[1,2,3],fn]
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+
+// console.log(arr.find(2)); //expects a function
+// console.log(arr.includes(100))
+const n  = arr.length // 5
+arr[arr.length - 1]
+
+// return nth element from last 
+arr.at(-n);
+
+// works similar to arr[index]
+// arr.at(index);
+
+// function as a parameter, callback
+
+// to get a new array applying a function on every element
+const squareNums =  arr.map( value=>value*value);
+
+arr.forEach((value,index)=>{
+    console.log('value is',value );
+})
+
+// filter
+// 1 1%2 => 1, 2 2%2=> 0 , 3 3%2 => 1
+// [1,3]
+// const even = arr.filter(value=>!(value%2))
+const even = arr.filter(value=>value%2===0);
+console.log(even);
+
+// const evenSquare = even.map(value=>value*value);
+const evenSquare = arr.filter(value=>!(value%2)).map(value=>value*value);
+console.log({evenSquare});
+
+
+
+const sumOfNumbers = arr.reduce((accumulator,currentValue)=>{
+    console.log(accumulator,currentValue,accumulator+currentValue);
+    accumulator+=currentValue;
+    return accumulator;
+},0);
+
+console.log({sumOfNumbers});
+
+
+// chaining
+// sum of all even square of number
+// const evenSquareSum = evenSquare.reduce((sum,val)=>sum+val);
+// const evenSquareSum = arr.filter(val=>!(val%2)).map(val=>val*val).reduce((sum,val)=>sum+val);
+const evenSquareSum = arr.filter(val=>!(val%2)).map(val=>val*val).reduce((sum,val)=>sum+val);
+console.log({evenSquareSum});
+console.clear();
+const arr2 = [...arr];
+// arr2 is copy of all values of arr
+arr2[0] = 999;
+console.log({arr},{arr2});
+
+const nestedArray = [[0,1],[2,3],[4,5]];
+const copyNestedArray = [...nestedArray];
+
+// copyNestedArray[0][0] = 999;
+copyNestedArray[0] = ["a","b"];
+console.log(nestedArray);
+console.log(copyNestedArray);
+
+
+// objects
+
+
+const employee = {
+    name:"abc",
+    designation:"Engg",
+    dept:{
+        name:"Core",
+        intro:()=>{
+            console.log(this)
+            return `Hi ${this.name}`
+        }
+    },
+    intro:()=>{
+        console.log(this)
+        return `Hi ${this.name}`
+    },
+    giveIntro(){
+        const fn = ()=>{
+            console.log('arrow fn',this);
+        }
+        console.log("outside fn",this);
+        fn();
+        return `Hi ${this.name}`
+    }
+}
+
+employee.designation;
+employee['designation'];
+employee.dept.intro();
+
+const empolyee2 = {...employee,name:"emp2Name"};
+const empolyeeDeepCopy = {...employee,dept:{...employee.dept}};
+console.clear();
+empolyeeDeepCopy.dept.name = "xyz";
+empolyee2.dept.name = "xyz";
+console.log(employee);
+console.log(empolyeeDeepCopy);
+console.log(Object.keys(employee));
+console.log(Object.values(employee));
+// combination of key and values
+console.log(Object.entries(employee));
+// console.log(employee.giveIntro());
+
+class Employee{
+    constructor(name,dept){
+        this.name = name;
+        this.dept = dept
+    }
+    intro(){
+        console.log(`hello I'm ${this.name} working in dept ${this.dept}`);
+    }
+}
+
+const emp1 = new Employee("abc","abc")
+const emp2 = new Employee("xyz","xyz")
+emp1.name
+
+
+emp1.intro();
+
+class Manager extends Employee{
+    constructor(name,dept,team){
+        super(name,dept);
+        this.team = team
+    }
+}
+
+
+const mgr = new Manager("manager 1","abc",[emp1,emp2]);
+console.log(mgr);
