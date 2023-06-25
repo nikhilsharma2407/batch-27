6b79cf52bda577a2e04be9442283cd90f11ada80
Nikhil Sharma <nikhil.sharma2407@gmail.com>
2023-06-25 22:05:16 +0530

    deploy commit

--- client/src/Signup/index.jsx
+++ client/src/Signup/index.jsx
@@ -16,60 +16,7 @@ function Signup() {
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
 
-  const [valid, setValid] = useState({
-    isNumberValid: false,
-    isLowerCaseValid: false,
-    isUpperCaseValid: false,
-    isSymbolValid: false,
-    isLengthValid: false,
-  });
-
-
-  const validate = (e) => {
-    const pwd = e.target.value;
-    // PATTERN.test(testString);
-    const NUM_PAT = /\d/;
-    const LOWER_CASE_PAT = /[a-z]/;
-    const UPPER_CASE_PAT = /[A-Z]/;
-    const SYMBOL_PAT = /[\W_]/;
-
-
-    const isLengthValid = pwd.length >= 8;
-
-    const isNumberValid = NUM_PAT.test(pwd);
-    const isLowerCaseValid = LOWER_CASE_PAT.test(pwd);
-    const isUpperCaseValid = UPPER_CASE_PAT.test(pwd);
-    const isSymbolValid = SYMBOL_PAT.test(pwd)
-
-
-    console.log({
-      isNumberValid,
-      isLowerCaseValid,
-      isUpperCaseValid,
-      isSymbolValid,
-      isLengthValid
-    });
-
-    setValid({
-      isNumberValid,
-      isLowerCaseValid,
-      isUpperCaseValid,
-      isSymbolValid,
-      isLengthValid
-    });
-
-
-    setPassword(pwd);
-
-  };
-
-  const { isNumberValid,
-    isLowerCaseValid,
-    isUpperCaseValid,
-    isSymbolValid,
-    isLengthValid } = valid
-
-  const isFormValid = Object.values(valid).every(Boolean) && [name, username, password].every(Boolean);
+  const isFormValid = [name, username, password].every(Boolean);
 
   const signup = (e) => {
     e.preventDefault();
@@ -112,20 +59,12 @@ function Signup() {
 
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={validate} />
+                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
                   </Form.Group>
                   <Button variant="primary" type="submit" disabled={!isFormValid} onClick={signup}>
                     Submit
                   </Button>
                 </Form>
-
-                <section>
-                  <div className={isNumberValid ? 'valid' : 'invalid'}>Atleast 1 number</div>
-                  <div className={isLowerCaseValid ? 'valid' : 'invalid'}>Lowercase letter</div>
-                  <div className={isUpperCaseValid ? 'valid' : 'invalid'}>Uppercase letter</div>
-                  <div className={isSymbolValid ? 'valid' : 'invalid'}>Special character</div>
-                  <div className={isLengthValid ? 'valid' : 'invalid'}>Atleast 8 characters</div>
-                </section>
               </>}
 
 
